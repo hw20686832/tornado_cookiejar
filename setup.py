c0f3f57666291c2111445500dc406a2be03025e6
huangwei <huangwei@weiyankeji.cn>
2018-12-28 14:40:29 +0800

    update

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='tornado_cookiejar',
     packages=['tornado_cookiejar'],
-    version='0.1.0',
+    version='0.1.1',
     description='Cookiejar with AsyncHTTPClient support on tornado',
     author='David Wong',
     author_email='stef-hw@163.com',
     url='https://github.com/hw20686832/tornado_cookiejar',
-    download_url='https://github.com/hw20686832/tornado_cookiejar/archive/v0.1.tar.gz',
+    download_url='https://github.com/hw20686832/tornado_cookiejar/archive/v0.1.1.tar.gz',
     keywords=['tornado', 'cookiejar', 'cookie'],
     classifiers=[],
 )
